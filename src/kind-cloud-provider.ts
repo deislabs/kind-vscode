634f5c15bbee7d29aef778039a1bfdf4fe7b2611
Ivan Towlson <itowlson@microsoft.com>
2019-07-22 15:03:50 +1200

    Delete Cluster command

--- src/kind-cloud-provider.ts
+++ src/kind-cloud-provider.ts
@@ -17,17 +17,17 @@ class KindCloudProvider implements k8s.CloudExplorerV1.CloudProvider {
     }
 }
 
-interface KindCloudProviderClusterNode {
+export interface KindCloudProviderClusterNode {
     readonly nodeType: 'cluster';
     readonly clusterName: string;
 }
 
-interface KindCloudProviderErrorNode {
+export interface KindCloudProviderErrorNode {
     readonly nodeType: 'error';
     readonly diagnostic: string;
 }
 
-type KindCloudProviderTreeNode = KindCloudProviderClusterNode | KindCloudProviderErrorNode;
+export type KindCloudProviderTreeNode = KindCloudProviderClusterNode | KindCloudProviderErrorNode;
 
 class KindTreeDataProvider implements vscode.TreeDataProvider<KindCloudProviderTreeNode> {
     private onDidChangeTreeDataEmitter: vscode.EventEmitter<KindCloudProviderTreeNode | undefined> = new vscode.EventEmitter<KindCloudProviderTreeNode | undefined>();
